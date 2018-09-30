268dd7581a53cdbbf1295ad044a16669a60be17f
Brandon Head <bhead43@gmail.com>
2018-09-30 15:27:21 -0500

    add collision framework to tilemap, put characterSpriteSheet.png and paperBall.png into assets folder

--- JanComb/level1.js
+++ JanComb/level1.js
@@ -1,18 +1,38 @@
 var demo = {};
 
+var blockLayer, goalLayer;
+
 demo.level1 = function(){};
 demo.level1.prototype = {
 	preload: function(){
         game.load.tilemap('levelOne', 'assets/levelMap.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('Tiles', 'assets/protoTileSet.png');
+        //Also load in the character sprite sheet and something to be the trash block for now
+        //  (There should be a paperBall.png in the assets folder that you could use)
     },
 	create: function(){
+        //Start Physics
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        //Add tilemap and layers to state
         var map = game.add.tilemap('levelOne');
         map.addTilesetImage('Tiles');
         
         var baseLayer = map.createLayer('Floor');
-        var blockLayer = map.createLayer('Blocks');
-        var goalLayer = map.createLayer('Goal');
+        blockLayer = map.createLayer('Blocks');
+        goalLayer = map.createLayer('Goal');
+        
+        //Set collision on the 'Blocks' layer
+        map.setCollisionBetween(2, 2, true, 'Blocks');
+        
+        //MIGHT need to uncomment this to have some detection when the block hits the goal area
+        //map.setCollisonBetween(3, 3, true, 'Goal');
     },
-	update: function(){}
+	update: function(){
+        //Fill these in with actual variables instead of the placeholders in there now
+        //----------------------------------------------------------------------------
+        //game.physics.arcade.collide(PLAYER_VARIABLE_HERE, blockLayer);
+        //game.physics.arcade.collide(TRASH_BLOCK_VARIABLE_HERE, blockLayer);
+        
+    }
 };
