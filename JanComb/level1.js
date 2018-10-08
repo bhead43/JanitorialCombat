0f2ce0f61afe111df0512653e80bc3646260296a
htebannah <43012710+htebannah@users.noreply.github.com>
2018-10-07 21:13:43 -0400

    Update level1.js

--- JanComb/level1.js
+++ JanComb/level1.js
@@ -10,7 +10,7 @@ demo.level1.prototype = {
         game.load.image('Tiles', 'assets/protoTileSet.png');
         game.load.spritesheet('jan', 'assets/characterSpritesheet.png', 230, 405);
         game.load.image('Trash', 'assets/paperBall.png'); // for now
-        game.load.spritesheet('villain', 'assets/villainSpritesheet.png', 230,405);
+        game.load.spritesheet('villain', 'assets/villainSpritesheet.png', 300,300,6);
         
 		
     },
