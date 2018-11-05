8de62aebdf27e248158ac7f39d483eafdf25800a
Carlos De La Rosa <delarosacarlos04@users.noreply.github.com>
2018-11-05 17:04:26 -0600

    Add files via upload

---
+++ level2.js
@@ -0,0 +1,10 @@
+demo.level2 = function(){};
+demo.level2.prototype = {
+	preload: function(){},
+	create: function(){
+		game.stage.backgroundColor = '#522122';
+
+		
+	},
+	update: function(){}
+};
