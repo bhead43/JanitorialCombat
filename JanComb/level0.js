bc0fcba848529b16aaeb755e8bcee67790095c4e
Brandon Head <bhead43@gmail.com>
2018-11-05 11:56:44 -0600

    More cleaning

--- JanComb/level0.js
+++ JanComb/level0.js
@@ -67,156 +67,8 @@ demo.level0.prototype = {
     },
 	update: function(){
      	//Variable to hold current animation
-        var currentAnim;
-        //Collision checks
-        var hitGoal = game.physics.arcade.collide(trash.trash, goalLayer);
-        var badHit = game.physics.arcade.collide(villain, jan);
-	    var hitWall = game.physics.arcade.collide(trash.trash, blockLayer);
-        
-	    // Basic collisions
-        game.physics.arcade.collide(jan, blockLayer);
-        game.physics.arcade.collide(trash.trash, blockLayer);
-        game.physics.arcade.collide(trash.trash, goalLayer)
-        game.physics.arcade.collide(villain, blockLayer)
-        
-        // Trash collision with player
-        var upCollide = game.physics.arcade.collide(jan, trash.upChild); 
-        var downCollide = game.physics.arcade.collide(jan, trash.downChild);
-        var leftCollide = game.physics.arcade.collide(jan, trash.leftChild);
-        var rightCollide = game.physics.arcade.collide(jan, trash.rightChild);
-        
-        // Trash collision with monster
-        var upBadCollide = game.physics.arcade.collide(villain, trash.upChild);
-        var downBadCollide = game.physics.arcade.collide(villain, trash.downChild);
-        var leftBadCollide = game.physics.arcade.collide(villain, trash.leftChild);
-        var rightBadCollide = game.physics.arcade.collide(villain, trash.rightChild);
-
-        // Janitor movement
-        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-		    jan.body.velocity.y = 0;
-		    jan.body.velocity.x = velocity;
-           	jan.animations.play('walkRight', 7, false);
-        }
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-            jan.body.velocity.y = 0;
-            jan.body.velocity.x = velocity * -1;
-            jan.animations.play('walkLeft', 7, false);
-        }
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-            jan.body.velocity.x = 0;
-            jan.body.velocity.y = velocity * -1;
-            jan.animations.play('walkUp', 7, false);
-        }
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-            jan.body.velocity.x = 0;
-            jan.body.velocity.y = velocity;
-            jan.animations.play('walkDown', 7, false);
-        }
-        else{
-//            jan.animations.stop();
-//            jan.frame = 0
-            //currentAnim = jan.animations.currentAnim.name;
-            //while (jan.animations.currentAnim.isFinished)
-            jan.body.velocity.x = 0;
-            jan.body.velocity.y = 0;
-        }
-	
-        // Trash monster movement
-        if(villain.body.velocity.y > 0){
-            villain.animations.play('walkDown', 7, true);
-        }
-        else if(villain.body.velocity.x < 0){
-            villain.animations.play('walkLeft', 7, true);
-        } 
-        else if(villain.body.velocity.x > 0){
-            villain.animations.play('walkRight', 7, true);
-        }
-        else {
-            villain.animations.play('walkUp', 7, true);
-        }
-        
-        // Trash movement
-	    // PUSH
-        // First check to see if the 'E' key is pressed...
-        if(game.input.keyboard.isDown(Phaser.Keyboard.E)){
-	    // Now collisions
-            if(upCollide){	// move trash down
-                jan.animations.play('pushDown', 1, false); 
-  		        trash.trash.body.velocity.y = trashVelocity;
-            }
-            else if(downCollide){	// move trash up
-                jan.animations.play('pushUp', 3, false);
-                trash.trash.body.velocity.y = trashVelocity * -1;
-            }
-            else if(leftCollide){	// move trash right
-                jan.animations.play('pushRight', 3, false);
-                trash.trash.body.velocity.x = trashVelocity;
-            }
-            else if(rightCollide){	// move trash left
-                jan.animations.play('pushLeft', 3, false);
-                trash.trash.body.velocity.x = trashVelocity * -1;
-            }
-//            else{
-//                jan.animations.play('pushDown', 3, false);
-//            }
-        }
-		
-       // PULL
-       // Check to see if the 'F' key is pressed...
-       if(game.input.keyboard.isDown(Phaser.Keyboard.F)){
-            console.log(totalMove)
-            if(upCollide && totalMove < 40){	// pull trash up
-                trash.trash.position.y = trash.trash.position.y - 5;
-                totalMove++;
-            }
-            else if(downCollide && totalMove < 40){	// pull trash down
-                trash.trash.position.y = trash.trash.position.y + 5;
-                totalMove++;
-            }
-            else if(leftCollide && totalMove < 40){	// pull trash left
-                trash.trash.position.x = trash.trash.position.x - 5;
-                totalMove++;
-            }
-            else if(rightCollide && totalMove < 40){	// pull trash right
-                trash.trash.position.x = trash.trash.position.x + 5;
-                totalMove++;
-            }
-        }
-		
-        // Goal Detection
-        // Ends level once the trash ball hits the goal area
-        if(hitGoal){
-            trash.trash.kill();
-            bgMusic.stop();	// stop background music
-            totalMove = 0;
-            game.state.start('nextLevel');     
-        }
-        
-	    // Monster actions
-        // Moves trash monster continuously towards the janitor
-        game.physics.arcade.moveToObject(villain, jan, 75);
-		
-        // Check for collision with janitor
-        if(badHit){
-            jan.kill();
-            bgMusic.stop();	// stop background music
-            totalMove = 0;
-            game.state.start('gameOver');
-        }
-         
-        // Check for collision with trash ball children
-        if(upBadCollide){	// move trash down
-            trash.trash.body.velocity.y = trashVelocity;
-        }
-        else if(downBadCollide){	// move trash up
-            trash.trash.body.velocity.y = trashVelocity * -1;
-        }
-        else if(leftBadCollide){	// move trash to the right
-            trash.trash.body.velocity.x = trashVelocity;
-        }
-        else if(rightBadCollide){	// move trash to the left
-            trash.trash.body.velocity.x = trashVelocity * -1;
-        }
+        //  --All handled via function now (see bottom of file)
+        setupUpdate(jan, trash, villain, blockLayer, goalLayer);
     },
     //DON'T DELETE THIS STUFF! Uncomment it all if you need to check what the trash is doing in regards to collision boxes and all that
     render: function(){
@@ -285,6 +137,159 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+// Update setup function
+//  --Should hold damn near everything needed in the update loop
+function setupUpdate(jan, trash, villain, blockLayer, goalLayer){
+    var hitGoal = game.physics.arcade.collide(trash.trash, goalLayer);
+    var badHit = game.physics.arcade.collide(villain, jan);
+    var hitWall = game.physics.arcade.collide(trash.trash, blockLayer);
+        
+    // Basic collisions
+    game.physics.arcade.collide(jan, blockLayer);
+    game.physics.arcade.collide(trash.trash, blockLayer);
+    game.physics.arcade.collide(trash.trash, goalLayer)
+    game.physics.arcade.collide(villain, blockLayer)
+        
+    // Trash collision with player
+    var upCollide = game.physics.arcade.collide(jan, trash.upChild); 
+    var downCollide = game.physics.arcade.collide(jan, trash.downChild);
+    var leftCollide = game.physics.arcade.collide(jan, trash.leftChild);
+    var rightCollide = game.physics.arcade.collide(jan, trash.rightChild);
+        
+    // Trash collision with monster
+    var upBadCollide = game.physics.arcade.collide(villain, trash.upChild);
+    var downBadCollide = game.physics.arcade.collide(villain, trash.downChild);
+    var leftBadCollide = game.physics.arcade.collide(villain, trash.leftChild);
+    var rightBadCollide = game.physics.arcade.collide(villain, trash.rightChild);
+    
+    // Janitor movement
+    if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        jan.body.velocity.y = 0;
+        jan.body.velocity.x = velocity;
+        jan.animations.play('walkRight', 7, false);
+    }
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+        jan.body.velocity.y = 0;
+        jan.body.velocity.x = velocity * -1;
+        jan.animations.play('walkLeft', 7, false);
+    }
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+        jan.body.velocity.x = 0;
+        jan.body.velocity.y = velocity * -1;
+        jan.animations.play('walkUp', 7, false);
+    }
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+        jan.body.velocity.x = 0;
+        jan.body.velocity.y = velocity;
+        jan.animations.play('walkDown', 7, false);
+    }
+    else{
+//        jan.animations.stop();
+//        jan.frame = 0
+//        currentAnim = jan.animations.currentAnim.name;
+//        while (jan.animations.currentAnim.isFinished)
+        jan.body.velocity.x = 0;
+        jan.body.velocity.y = 0;
+    }
+    
+    // Trash monster movement
+    if(villain.body.velocity.y > 0){
+        villain.animations.play('walkDown', 7, true);
+    }
+    else if(villain.body.velocity.x < 0){
+        villain.animations.play('walkLeft', 7, true);
+    } 
+    else if(villain.body.velocity.x > 0){
+        villain.animations.play('walkRight', 7, true);
+    }
+    else {
+        villain.animations.play('walkUp', 7, true);
+    }
+    
+    // Trash movement
+    // PUSH
+    // First check to see if the 'E' key is pressed...
+    if(game.input.keyboard.isDown(Phaser.Keyboard.E)){
+    // Now collisions
+        if(upCollide){	// move trash down
+            jan.animations.play('pushDown', 1, false); 
+  		    trash.trash.body.velocity.y = trashVelocity;
+        }
+        else if(downCollide){	// move trash up
+            jan.animations.play('pushUp', 3, false);
+            trash.trash.body.velocity.y = trashVelocity * -1;
+        }
+        else if(leftCollide){	// move trash right
+            jan.animations.play('pushRight', 3, false);
+            trash.trash.body.velocity.x = trashVelocity;
+        }
+        else if(rightCollide){	// move trash left
+            jan.animations.play('pushLeft', 3, false);
+            trash.trash.body.velocity.x = trashVelocity * -1;
+        }
+//        else{
+//            jan.animations.play('pushDown', 3, false);
+//        }
+    }
+    
+    // PULL
+    // Check to see if the 'F' key is pressed...
+    if(game.input.keyboard.isDown(Phaser.Keyboard.F)){
+        console.log(totalMove)
+        if(upCollide && totalMove < 40){	// pull trash up
+            trash.trash.position.y = trash.trash.position.y - 5;
+            totalMove++;
+        }
+        else if(downCollide && totalMove < 40){	// pull trash down
+            trash.trash.position.y = trash.trash.position.y + 5;
+            totalMove++;
+        }
+        else if(leftCollide && totalMove < 40){	// pull trash left
+            trash.trash.position.x = trash.trash.position.x - 5;
+            totalMove++;
+        }
+        else if(rightCollide && totalMove < 40){	// pull trash right
+            trash.trash.position.x = trash.trash.position.x + 5;
+            totalMove++;
+        }
+    }
+    
+    // Goal Detection
+    // Ends level once the trash ball hits the goal area
+    if(hitGoal){
+        trash.trash.kill();
+        bgMusic.stop();	// stop background music
+        totalMove = 0;
+        game.state.start('nextLevel');     
+    }
+    
+    // Monster actions
+    // Moves trash monster continuously towards the janitor
+    game.physics.arcade.moveToObject(villain, jan, 75);
+    
+    // Check for collision with janitor
+    if(badHit){
+        jan.kill();
+        bgMusic.stop();	// stop background music
+        totalMove = 0;
+        game.state.start('gameOver');
+    }
+    
+    // Check for collision with trash ball children
+    if(upBadCollide){	// move trash down
+        trash.trash.body.velocity.y = trashVelocity;
+    }
+    else if(downBadCollide){	// move trash up
+        trash.trash.body.velocity.y = trashVelocity * -1;
+    }
+    else if(leftBadCollide){	// move trash to the right
+        trash.trash.body.velocity.x = trashVelocity;
+    }
+    else if(rightBadCollide){	// move trash to the left
+        trash.trash.body.velocity.x = trashVelocity * -1;
+    }
 }
 
 // Checks overlap between sprites
