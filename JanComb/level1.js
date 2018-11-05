370697b0ffcfac905f010761e1a178ceed2868b5
htebannah <43012710+htebannah@users.noreply.github.com>
2018-11-05 00:26:36 -0500

    Update level1.js

--- JanComb/level1.js
+++ JanComb/level1.js
@@ -108,182 +108,120 @@ demo.level1.prototype = {
         var leftBadCollide = game.physics.arcade.collide(villain, leftChild);
         var rightBadCollide = game.physics.arcade.collide(villain, rightChild);
         
-        //
+		
+        // Janitor movement
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-			//jan.scale.setTo(0.2, 0.2);
-            jan.body.velocity.y = 0;
-			jan.body.velocity.x = velocity;
-            jan.animations.play('walkRight', 7, true);
-	    villain.animations.play('walkRight', 7, true);
-		}
-		else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-			//jan.scale.setTo(-0.2, 0.2);
-            jan.body.velocity.y = 0;
-			jan.body.velocity.x = velocity * -1;
-            jan.animations.play('walkLeft', 7, true);
-	    villain.animations.play('walkLeft', 7, true);
-		}
-
-		else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-            //jan.scale.setTo(0.2, 0.2);
-            jan.body.velocity.x = 0;
-			jan.body.velocity.y = velocity * -1;
-            jan.animations.play('walkUp', 7, true);
-	    villain.animations.play('walkUp', 7, true);
-			
-		}
-		else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-            //jan.scale.setTo(0.2, 0.2);
-            jan.body.velocity.x = 0;
-			jan.body.velocity.y = velocity;
-            jan.animations.play('walkDown', 7, true);
-	    villain.animations.play('walkDown', 7, true);
-		}
-	   
-        else{
-            jan.animations.stop();
-            jan.frame = 0
-            jan.body.velocity.x = 0;
-            jan.body.velocity.y = 0;
+		jan.body.velocity.y = 0;
+		jan.body.velocity.x = velocity;
+		jan.animations.play('walkRight', 7, true);
+	}
+	else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+		jan.body.velocity.y = 0;
+		jan.body.velocity.x = velocity * -1;
+           	jan.animations.play('walkLeft', 7, true);
+	}
+	else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+ 		jan.body.velocity.x = 0;
+		jan.body.velocity.y = velocity * -1;
+            	jan.animations.play('walkUp', 7, true);
+	}
+	else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+		jan.body.velocity.x = 0;
+		jan.body.velocity.y = velocity;
+            	jan.animations.play('walkDown', 7, true);
+	}
+	else{
+		jan.animations.stop();
+		jan.frame = 0
+            	jan.body.velocity.x = 0;
+            	jan.body.velocity.y = 0;
         }
 		
-		
+	// Trash monster movement animations	
 	 if(villain.body.velocity.y > 0){
-             villain.animations.play('walkDown', 7, true);
+		 villain.animations.play('walkDown', 7, true);
         }
         else if(villain.body.velocity.x < 0){
-            villain.animations.play('walkLeft', 7, true);
+		villain.animations.play('walkLeft', 7, true);
         } else if(villain.body.velocity.x > 0){
-            villain.animations.play('walkRight', 7, true);
+		villain.animations.play('walkRight', 7, true);
         }
         else {
-            villain.animations.play('walkUp', 7, true);
+		villain.animations.play('walkUp', 7, true);
         }
-        
-        //Trash movement!
-        //  What *should* happen:
-        //      -Only gets moved when you hit a button (making it 'E' for now) while next to the ball
-        //      -Direction of movement depends on where you hit it from
-        //      -Only ever moves on one axis
-        
-        //First check to see if the 'E' key is pressed...
+ 
+		
+	// Trash movement
+	// PUSH
+        // First check to see if the 'E' key is pressed...
         if(game.input.keyboard.isDown(Phaser.Keyboard.E)){
-            if(upCollide){
-                //Put a pushing animation here! At some point
-                //PUSH_DOWN ANIMATION GOES HERE
+            if(upCollide){	// move trash down
                 jan.animations.play('pushDown', 3, false);
-                
-                //Move trash down
                 trash.body.velocity.y = trashVelocity;
             }
-            else if(downCollide){
-                //PUSH_UP ANIMATION GOES HERE
+            else if(downCollide){	// move trash up
                 jan.animations.play('pushUp', 3, false);
-                
-                //Move trash up
-                trash.body.velocity.y = trashVelocity * -1;
+		trash.body.velocity.y = trashVelocity * -1;
             }
-            else if(leftCollide){
-                //PUSH_RIGHT ANIMATION GOES HERE
+            else if(leftCollide){	// move trash right
                 jan.animations.play('pushRight', 3, false);
-                
-                //Move trash to the right
                 trash.body.velocity.x = trashVelocity;
             }
-            else if(rightCollide){
-                //PUSH_LEFT ANIMATION GOES HERE
+            else if(rightCollide){	// move trash left
                 jan.animations.play('pushLeft', 3, false);
-                
-                //Move trash to the left
                 trash.body.velocity.x = trashVelocity * -1;
             }
             else{
-                //PUSH_BAD ANIMATION GOES HERE
                 jan.animations.play('pushDown', 3, false);
             }
         }
         
+	// PULL
         //Check to see if the 'F' key is pressed...
         if(game.input.keyboard.isDown(Phaser.Keyboard.F)){
             console.log(totalMove)
-            if(upCollide && totalMove < 20){
-                //Put a pulling animation here! At some point
-                //PULL_UP ANIMATION GOES HERE
-                
-                //Pull trash up
+            if(upCollide && totalMove < 20){	// pull trash up
                 trash.position.y = trash.position.y - 5;
                 totalMove++;
             }
-            else if(downCollide && totalMove < 20){
-                //PULL_DOWN ANIMATION GOES HERE
-                
-                //Pull trash down
+            else if(downCollide && totalMove < 20){	// pull trash down
                 trash.position.y = trash.position.y + 5;
                 totalMove++;
             }
-            else if(leftCollide && totalMove < 20){
-                //PULL_LEFT ANIMATION GOES HERE
-                
-                //Pull trash to the left
-                trash.position.x = trash.position.x - 5;
+            else if(leftCollide && totalMove < 20){	// pull trash left
+		trash.position.x = trash.position.x - 5;
                 totalMove++;
             }
-            else if(rightCollide && totalMove < 20){
-                //PULL_RIGHT ANIMATION GOES HERE
-                
-                //Pull trash to the right
+            else if(rightCollide && totalMove < 20){	// pull trash right
                 trash.position.x = trash.position.x + 5;
                 totalMove++;
             }
             else{
-                //PULL_BAD ANIMATION GOES HERE
             }
         }
         
         //Goal Detection
-        //  Ends level once the trash ball hits the goal area
         if(hitGoal){
             trash.kill();
-            bgMusic.pause();
+            bgMusic.pause();	// pause background music
             totalMove = 0;
-            game.state.start('nextLevel');  
+            game.state.start('nextLevel');	// move onto next level
         }
 
-        //Enemy movement!
-        //  What *should* happen:
-        //      -Enemy moves towards the player at all times
-        //      -If the enemy collides with the player, deal damage!
-        //          --Will probably just be a 'game over' and reset the level on hit for now. Will implement an HP mechanic later on
-        //      -If the enemy collides with the ball, it'll move the ball
-        //          --Will follow the same rules as the player; if it hits from the bottom, move it up, etc.
-        
-        //Moves the villain sprite continuously towards the jan sprite
+      
+        // Moves the villain sprite continuously towards the jan sprite
         game.physics.arcade.moveToObject(villain, jan, 75);
-        
-        
-      //  if(jan.body.velocity.y < 0){
-         //    villain.animations.play('walkUp', 7, true);
-       // }
-        //else if(jan.body.velocity.x < 0){
-          //  villain.animations.play('walkLeft', 7, true);
-        //} else if(jan.body.velocity.x > 0){
-          //  villain.animations.play('walkRight', 7, true);
-        //}
-        //else {
-          //  villain.animations.play('walkDown', 7, true);
-      //  }       
-        
-        //Check for collision with the character
-        //  --Might just send this to a separate state? Not sure yet
+   
+        // Check for collision with janitor
         if(badHit){
-            //Once hit, game over! Put some text up and prompt the player to restart the level
+            // Game over
             jan.kill();
-            bgMusic.pause();
+            bgMusic.pause();	// pause background music
             totalMove = 0;
             game.state.start('gameOver');
         }
          
-        //Check for collision with trash ball children
+        // Check for collision with trash ball children
             if(upBadCollide){	// move trash down
                 trash.body.velocity.y = trashVelocity;
             }
